cd9463a148594305aae1925c6de56682f4cdba44
amandaliii <125772721+amandaliii@users.noreply.github.com>
2025-07-02 22:14:26 -0400

    Update DataProcessing.py

--- DataProcessing.py
+++ DataProcessing.py
@@ -3,33 +3,33 @@ from datasets import Dataset
 import os
 
 # LOAD AND CLEAN DATA
-def load_mimic3_data(mimic_3data):
+def load_mimic3_data(mimic_3data, max_hadm_ids=100):
     # INPUT DATA - READ CSV.GZ FILES
-    admissions = pd.read_csv(os.path.join(mimic_3data, "ADMISSIONS.csv.gz"), compression="gzip")
-    chart_events = pd.read_csv(os.path.join(mimic_3data, "CHARTEVENTS.csv.gz"), compression="gzip")
-    lab_events = pd.read_csv(os.path.join(mimic_3data, "LABEVENTS.csv.gz"), compression="gzip")
-    note_events = pd.read_csv(os.path.join(mimic_3data, "NOTEVENTS.csv.gz"), compression="gzip")
-    patients = pd.read_csv(os.path.join(mimic_3data, "PATIENTS.csv.gz"), compression="gzip")
+    admissions = pd.read_csv(os.path.join(mimic_3data, "ADMISSIONS.csv.gz"), compression="gzip", nrows=100)
+    chart_events = pd.read_csv(os.path.join(mimic_3data, "CHARTEVENTS.csv.gz"), compression="gzip", nrows=100)
+    lab_events = pd.read_csv(os.path.join(mimic_3data, "LABEVENTS.csv.gz"), compression="gzip", nrows=100)
+    note_events = pd.read_csv(os.path.join(mimic_3data, "NOTEEVENTS.csv.gz"), compression="gzip", nrows=100)
+    patients = pd.read_csv(os.path.join(mimic_3data, "PATIENTS.csv.gz"), compression="gzip", nrows=100)
 
     # OUTPUT DATA - READ CSV.GZ FILES
-    diagnoses_icd = pd.read_csv(os.path.join(mimic_3data, "DIAGNOSES_ICD.csv.gz"), compression="gzip")
-    prescriptions = pd.read_csv(os.path.join(mimic_3data, "PRESCRIPTIONS.csv.gz"), compression="gzip")
-    procedures_icd = pd.read_csv(os.path.join(mimic_3data, "PROCEDURES_ICD.csv.gz"), compression="gzip")
+    diagnoses_icd = pd.read_csv(os.path.join(mimic_3data, "DIAGNOSES_ICD.csv.gz"), compression="gzip", nrows=100)
+    prescriptions = pd.read_csv(os.path.join(mimic_3data, "PRESCRIPTIONS.csv.gz"), compression="gzip", nrows=100)
+    procedures_icd = pd.read_csv(os.path.join(mimic_3data, "PROCEDURES_ICD.csv.gz"), compression="gzip", nrows=100)
 
     #OUTPUT DATA AGGREGATED
     # numerical data
     # aggregate diagnoses_icd (output) ICD9 codes/diagnoses per admission
-    diagnoses_agg = diagnoses_icd.groupby(['SUBJECT_ID', 'HADM_ID'])['ICV9_CODE'].agg(lambda x: ';'.join(x.dropna())).reset_index()
-    diagnoses_agg.rename(columns={'ICD9C_CODE': 'DIAGNOSES'}, inplace=True)
+    diagnoses_agg = diagnoses_icd.groupby(['SUBJECT_ID', 'HADM_ID'])['ICD9_CODE'].agg(lambda x: ';'.join(x.astype(str).dropna())).reset_index()
+    diagnoses_agg.rename(columns={'ICD9_CODE': 'DIAGNOSES'}, inplace=True)
 
     # aggregate procedures_icd (output) ICD9 codes/diagnoses per admission
-    procedures_agg = procedures_icd.groupby(['SUBJECT_ID', 'HADM_ID'])['ICV9_CODE'].agg(lambda x: ';'.join(x.dropna())).reset_index()
-    diagnoses_agg.rename(columns={'ICD9C_CODE': 'PROCEDURES'}, inplace=True)
+    procedures_agg = procedures_icd.groupby(['SUBJECT_ID', 'HADM_ID'])['ICD9_CODE'].agg(lambda x: ';'.join(x.astype(str).dropna())).reset_index()
+    procedures_agg.rename(columns={'ICD9_CODE': 'PROCEDURES'}, inplace=True)
 
     # text data
     # aggregate prescriptions; drug names per admission
-    prescriptions_agg = prescriptions.groupby(['SUBJECT_ID', 'HADM_ID'])['ICV9_CODE'].agg(lambda x: ';'.join(x.dropna())).reset_index()
-    diagnoses_agg.rename(columns={'ICD9C_CODE': 'PRESCRIPTIONS'}, inplace=True)
+    prescriptions_agg = prescriptions.groupby(['SUBJECT_ID', 'HADM_ID'])['DRUG'].agg(lambda x: ';'.join(x.astype(str).dropna())).reset_index()
+    prescriptions_agg.rename(columns={'DRUG': 'MEDICATIONS'}, inplace=True)
 
     # aggregate chart events
     chart_events_agg = chart_events.groupby(['SUBJECT_ID', 'HADM_ID']).agg({
@@ -47,7 +47,7 @@ def load_mimic3_data(mimic_3data):
 
     # aggregate note events
     note_events_agg = note_events.groupby(['SUBJECT_ID', 'HADM_ID'])['TEXT'].agg(lambda x: ' '.join(x.dropna())).reset_index()
-    note_events_agg.rename(columns={'ITEMID': 'NOTES'}, inplace=True)
+    note_events_agg.rename(columns={'TEXT': 'NOTES'}, inplace=True)
 
     # MERGE ALL TABLES WITH ADMISSION AS BASE
     merged_data = admissions[['SUBJECT_ID', 'HADM_ID', 'ADMITTIME', 'DIAGNOSIS']]
@@ -91,6 +91,6 @@ def load_mimic3_data(mimic_3data):
 
 if __name__ == "__main__":
     mimic_data = "/Users/amandali/Downloads/Mimic III"
-    full_data = load_mimic3_data(mimic_data)
+    full_data = load_mimic3_data(mimic_data, max_hadm_ids=100)
     if full_data is None:
         print("full data loading and processing complete. ")
